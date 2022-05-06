16af11f9370e692d4928eab339c584d379872d25
vessela991 <vspopova99@gmail.com>
2022-05-06 11:26:29 +0300

    Create java project for Tour Finder

---
+++ tourFinder/src/test/java/fmi/java/web/tourFinder/TourFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package fmi.java.web.tourFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
