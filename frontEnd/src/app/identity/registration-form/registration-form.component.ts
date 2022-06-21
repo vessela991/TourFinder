3d43eb11cb7e3c474dd5a68fda3621ce0a6617a9
Vesela Stoyanova <64095642+VeselaStoyanova@users.noreply.github.com>
2022-06-21 15:52:03 +0300

    Add registration validations

--- frontEnd/src/app/identity/registration-form/registration-form.component.ts
+++ frontEnd/src/app/identity/registration-form/registration-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
+import {AuthService} from "../services/auth.service";
 
 @Component({
   selector: 'app-registration-form',
@@ -17,13 +18,13 @@ export class RegistrationFormComponent implements OnInit{
   phoneNumber = new FormControl('');
   hide = true;
 
-  constructor(private router:Router, private formBuilder: FormBuilder) {
+  constructor(private router:Router, private formBuilder: FormBuilder, private authService:AuthService) {
     this.registrationForm = this.formBuilder.group({
-      'username' : ['', [Validators.required]],
-      'email' : ['', [Validators.required, Validators.email]],
-      'password': ['', Validators.required],
+      'username' : ['', [Validators.required, Validators.pattern('^[A-Za-z][A-Za-z0-9_]{5,29}$')]],
+      'email' : ['', [Validators.required, Validators.email, Validators.pattern('^(.+)@(.+)$')]],
+      'password': ['', Validators.required, Validators.pattern('^(?=.*[0-9])(?=.*[!@.#$%^&*])[a-zA-Z0-9!.@#$%^&*]{6,16}$')],
       'confirmedPassword' : ['', Validators.required],
-      'phoneNumber' : ['', [Validators.required]]
+      'phoneNumber' : ['', [Validators.required, Validators.pattern('^([0-9]{3}\\)[0-9]{3}-[0-9]{4}$')]]
     })
   }
 
@@ -31,18 +32,58 @@ export class RegistrationFormComponent implements OnInit{
   }
 
   register(){
+    if(!this.registrationForm.valid){
+      return;
+    }
+    this.authService.register(this.registrationForm.value).subscribe(data => {
+      console.log(data);
+    });
     return this.router.navigate(['/login']);
   }
 
   navigate(){
-    return this.router.navigate(['/registration']);
+    return this.router.navigate(['/login']);
   }
 
-  getErrorMessage() {
-    if (this.username.hasError('required') || this.email.hasError('required') ||
-      this.password.hasError('required') || this.confirmedPassword.hasError('required') ||this.phoneNumber.hasError('required')) {
-      return 'You must enter a value';
+  getUsernameErrorMessage() {
+    if(this.username.hasError('required')){
+      return 'You must enter an username!';
     }
-    return this.email.hasError('email') ? 'Not a valid email' : '';
+    return this.username.hasError('pattern' ?
+      'The username must start with capital letter and the length must be between 6 and 30 symbols!' : '');
+  }
+
+  getPasswordErrorMessage() {
+    if(this.password.hasError('required')){
+      return 'You must enter a password!';
+    }
+    return this.password.hasError('pattern' ?
+      'Password length must be between 7 and 17 symbols and password must contain one or more uppercase , lowercase , ' +
+      'numeric and special characters!' : '');
+
+  }
+
+  getConfirmPasswordErrorMessage() {
+    if(this.confirmedPassword.hasError('required')){
+      return 'You must confirm the password!';
+    }
+    return this.password !== this.confirmedPassword ?
+      'The password and the confirmed password are not the same!' : '';
+    }
+
+  getEmailErrorMessage() {
+    if(this.email.hasError('required')){
+      return 'You must enter an email!';
+    }else if(this.email.hasError('pattern')){
+      return 'You must enter a valid email!';
+    }
+    return this.email.hasError('email' ? 'Not a valid email!' : '');
+  }
+
+  getPhoneNumberErrorMessage() {
+    if(this.phoneNumber.hasError('required')){
+      return 'You must enter a phone number!';
+    }
+    return this.phoneNumber.hasError('pattern' ? 'Not a valid phone number!' : '');
   }
 }
