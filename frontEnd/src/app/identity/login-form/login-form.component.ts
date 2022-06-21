b24b8146d78a2e0649c104c4b5775d6a6059caf8
Vesela Stoyanova <64095642+VeselaStoyanova@users.noreply.github.com>
2022-06-21 15:49:50 +0300

    Add login validations

--- frontEnd/src/app/identity/login-form/login-form.component.ts
+++ frontEnd/src/app/identity/login-form/login-form.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {Router} from "@angular/router";
+import {AuthService} from "../services/auth.service";
 
 @Component({
   selector: 'app-login-form',
@@ -13,10 +14,14 @@ export class LoginFormComponent implements OnInit{
   password = new FormControl('');
   hide = true;
 
-  constructor(private router:Router, private formBuilder: FormBuilder) {
+  constructor(private router:Router, private formBuilder: FormBuilder, private authService:AuthService) {
     this.logInForm = this.formBuilder.group({
-      'email' : ['', [Validators.required, Validators.email]],
-      'password': ['', Validators.required]
+      'email' : ['', [Validators.required, Validators.email, Validators.pattern(
+        '^(.+)@(.+)$',
+      )]],
+      'password': ['', Validators.required, Validators.pattern(
+        '^((?=.*[0-9])(?=.*[!@.#$%^&*])[a-zA-Z0-9!.@#$%^&*]{6,16}$'
+      )]
     })
   }
 
@@ -28,15 +33,30 @@ export class LoginFormComponent implements OnInit{
   }
 
   login(){
-    //TODO: localStorage.setItem.....
+    if(!this.logInForm.valid){
+      return;
+    }
+    this.authService.login(this.logInForm.value).subscribe(data => {
+      localStorage.setItem('token', data.jwt);
+    })
     return this.router.navigate(['/home']);
   }
 
-  getErrorMessage() {
-    if (this.email.hasError('required') || this.password.hasError('required')) {
-      return 'You must enter a value';
+  getErrorEmailMessage() {
+    if(this.email.hasError('required')){
+      return 'You must enter an email!';
+    }else if(this.email.hasError('pattern')){
+      return 'You must enter a valid email!';
     }
-    return this.email.hasError('email') ? 'Not a valid email' : '';
+    return this.email.hasError('email' ? 'Not a valid email' : '');
   }
 
+  getErrorPasswordMessage(){
+    if(this.password.hasError('required')){
+      return 'You must enter a password!';
+    }
+    return this.password.hasError('pattern' ?
+      'Password length must be between 7 and 17 symbols and password must contain one or more uppercase , lowercase , ' +
+      'numeric and special characters!' : '');
+  }
 }
