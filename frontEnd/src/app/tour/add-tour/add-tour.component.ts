0242926e5b9020df2baf495c6713441c9ece5d14
Vesela Stoyanova <64095642+VeselaStoyanova@users.noreply.github.com>
2022-06-21 18:12:56 +0300

    Add validations for adding a tour

--- frontEnd/src/app/tour/add-tour/add-tour.component.ts
+++ frontEnd/src/app/tour/add-tour/add-tour.component.ts
@@ -9,20 +9,22 @@ import {Router} from "@angular/router";
 })
 export class AddTourComponent implements OnInit {
   addTourForm: FormGroup;
-  username = new FormControl('');
   tourName = new FormControl('');
   tourPrice = new FormControl('');
+  tourStartDate = new FormControl(new Date());
+  tourEndDate = new FormControl(new Date());
   tourDestination = new FormControl('');
   tourDescription = new FormControl('');
   constructor(private router:Router, private formBuilder: FormBuilder) {
     this.addTourForm = this.formBuilder.group({
-      'username' : ['', [Validators.required]],
       'tourName' : ['', [Validators.required]],
       'tourPrice' : ['', [Validators.required]],
+      'tourStartDate' : ['', Validators.required],
+      'tourEndDate' : ['', Validators.required],
       'tourDestination' : ['', [Validators.required]],
       'tourDescription' : ['', [Validators.required]],
     })
-    }
+  }
 
   ngOnInit(): void {
   }
@@ -31,11 +33,36 @@ export class AddTourComponent implements OnInit {
 
   }
 
-  getErrorMessage() {
-    if (this.username.hasError('required')) {
-      return 'You must enter a value';
-    }
-    return this.username.hasError('email') ? 'Not a valid email' : '';
+  getTourNameErrorMessage() {
+    return this.tourName.hasError('required') ? 'You must enter a name of the tour!' : '';
   }
 
+  getTourPriceErrorMessage() {
+    if(this.tourPrice.value <= 0){
+        return "The tour price must be greater than 0!";
+    }
+    return this.tourPrice.hasError('required') ? 'You must enter a price of the tour!' : '';
+  }
+
+  getTourStartDateErrorMessage(){
+    if(this.tourStartDate.value > new Date()){
+        return "The start date must be after the date of creating the tour!";
+    }
+    return this.tourStartDate.hasError('required') ? "You must enter a start date of the tour!" : '';
+  }
+
+  getTourEndDateErrorMessage(){
+    if(this.tourEndDate.value < this.tourStartDate.value){
+      return "The start date must be before end date!";
+    }
+    return this.tourEndDate.hasError('required') ? "You must enter an end date of the tour!" : '';
+  }
+
+  getTourDestinationErrorMessage() {
+    return this.tourDestination.hasError('required') ? 'You must enter a destination of the tour!' : '';
+  }
+
+  getTourDescriptionErrorMessage() {
+    return this.tourDescription.hasError('required') ? 'You must enter a description of the tour!' : '';
+  }
 }
